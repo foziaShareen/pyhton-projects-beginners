b48837289cc8c3fb7621cb96f8f403fa9227b9f9
foziaShareen <foziashareen1@gmail.com>
2024-07-23 09:20:45 +0500

    project birds story

---
+++ main.py
@@ -0,0 +1,30 @@
+# Bird-themed Mad Libs Generator - Charles Joseph Monaghan 11/10/2017
+
+# Loop back to this point once code finishes
+loop = 1
+
+while (loop < 2):
+
+    # All the questions that the program asks the user
+    bird = input("Choose a type of bird 🦜: ")
+    plural_bird = input("Choose a plural type of bird 🐦: ")
+    bird2 = input("Choose another type of bird 🐤: ")
+    place = input("Name a place 🌍: ")
+    adjective = input("Choose an adjective (Describing word) 🌟: ")
+    bird3 = input("Choose another type of bird 🦢: ")
+
+    # Displays the story based on the users input
+    print ("------------------------------------------")
+    print ("Be kind to your", bird, "- and", plural_bird)
+    print ("For a", bird2, "may be somebody's", bird2,",")
+    print ("Be kind to your", plural_bird, "in", place)
+    print ("Where the weather is always", adjective, ".")
+    print ()
+    print ("You may think that this is the", bird3, ",")
+    print ("Well it is.")
+    print ("------------------------------------------")
+    print("🐦🦜🦢🦉🕊️")
+
+
+    # Loop back to "loop = 1"
+    loop = loop + 1
